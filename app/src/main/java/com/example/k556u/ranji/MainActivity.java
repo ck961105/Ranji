c90513b6a7c6ef83fffb94d5ad250f108f8f0fdb
Andy <andy19950@gmail.com>
2017-04-11 15:58:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/k556u/ranji/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.k556u.ranji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+    }
+}
